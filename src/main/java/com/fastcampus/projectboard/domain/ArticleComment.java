3dc9a3b2026044c73b83284181f38d868f0f76c5
Jonathan <jonnygim.dev@gmail.com>
2022-11-29 23:33:54 +0900

    #42 - 강의 중 누락 된 것 같은 연관관계 매핑과 데이터 추가
    강의 중에 설명해주지 않은 부분은 코드 확인 후 추가함
    데이터도 변경 되었던 것 같아서 변경

--- src/main/java/com/fastcampus/projectboard/domain/ArticleComment.java
+++ src/main/java/com/fastcampus/projectboard/domain/ArticleComment.java
@@ -12,7 +12,7 @@ import java.util.Objects;
 @Table(indexes = {
         @Index(columnList = "content"),
         @Index(columnList = "createdAt"),
-        @Index(columnList = "createdBy"),
+        @Index(columnList = "createdBy")
 })
 @Entity
 public class ArticleComment extends AuditingFields {
@@ -22,8 +22,9 @@ public class ArticleComment extends AuditingFields {
     private Long id;
 
     @Setter @ManyToOne(optional = false) private Article article; // 게시글 (ID)
-    @Setter @ManyToOne(optional = false) private UserAccount userAccount; // 유저 정보 (ID)
-    @Setter @Column(nullable = false, length = 500) private String content;
+    @Setter @ManyToOne(optional = false) @JoinColumn(name = "userId") private UserAccount userAccount; // 유저 정보 (ID)
+
+    @Setter @Column(nullable = false, length = 500) private String content; // 본문
 
 
     protected ArticleComment() {}
@@ -49,4 +50,5 @@ public class ArticleComment extends AuditingFields {
     public int hashCode() {
         return Objects.hash(id);
     }
+
 }
